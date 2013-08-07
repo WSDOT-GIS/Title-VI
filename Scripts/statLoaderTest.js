53be0496df6a2b5bd81e95e670b3bfed09e2076e
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-08-07 16:31:23 -0700

    Added proxy.ashx.

--- Scripts/statLoaderTest.js
+++ Scripts/statLoaderTest.js
@@ -1,7 +1,9 @@
 ﻿/*global require*/
-require(["title6/statLoader"], function (StatLoader) {
+require(["esri/config", "title6/statLoader"], function (config, StatLoader) {
 	"use strict";
 	var statLoader;
+
+	config.defaults.io.proxyUrl = "proxy.ashx";
 
 	statLoader = new StatLoader("http://hqolymgis99t/arcgis/rest/services/Demographic/Language/MapServer/1");
 	statLoader.on("query-object-ids-complete", function (objectIds) {
