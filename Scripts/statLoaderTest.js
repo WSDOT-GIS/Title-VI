42206f08ff34856586183854a0f72b4360960d89
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-08-08 11:52:51 -0700

    Added event emitter for when ALL queries have completed.

--- Scripts/statLoaderTest.js
+++ Scripts/statLoaderTest.js
@@ -1,20 +1,35 @@
 ﻿/*global require*/
 require(["esri/config", "title6/statLoader"], function (config, StatLoader) {
 	"use strict";
-	var statLoader, featureSet;
+	var statLoader, featureSet, textArea;
+
+	textArea = document.getElementById("output");
 
 	config.defaults.io.proxyUrl = "proxy.ashx";
 
 	statLoader = new StatLoader("http://hqolymgis99t/arcgis/rest/services/Demographic/Language/MapServer/1");
 	statLoader.on("query-object-ids-complete", function (objectIds) {
-		console.log(objectIds);
+		console.log("Object IDs query complete", objectIds);
 	});
 
 	statLoader.on("query-object-ids-error", function (error) {
-		console.error(error);
+		console.error("Object IDs query error", error);
 	});
 
 	statLoader.on("query-group-complete", function (featureSet) {
-		
+		console.log("query group complete", featureSet);
+	});
+
+	statLoader.on("query-error", function (error) {
+		console.error("query-error", error);
+	});
+
+	statLoader.on("all-queries-complete", function (results) {
+		if (results.errorCount) {
+			console.log(["Queries completed with", results.errorCount, "errors"].join(" "));
+		} else {
+			console.log("All queries completed");
+		}
+		textArea.value += JSON.stringify(results.featureSet);
 	});
 });
--- Scripts/wsdot/title6/statLoader.js
+++ Scripts/wsdot/title6/statLoader.js
@@ -9,13 +9,48 @@ define([
 	"use strict";
 	var StatLoader;
 
+	/** Combines an array of feature sets with the same schema into a single feature set.
+	Note that the schema is not verified before combining.
+	@return {FeatureSet}
+	*/
+	function combineFeatureSets(/** {FeatureSet[]}*/ featureSets) {
+		var i, l, currentFeatureSet, output;
+		for (i = 0, l = featureSets.length; i < l; i += 1) {
+			currentFeatureSet = featureSets[i];
+			if (!output && currentFeatureSet && currentFeatureSet.features.length) {
+				output = currentFeatureSet;
+			} else {
+				if (currentFeatureSet.features && currentFeatureSet.features.length) {
+					output.features = output.features.concat(currentFeatureSet.features);
+				}
+			}
+		}
+		return output;
+	}
+
 	StatLoader = declare([Evented], {
 		/**
 		@param {String} url The URL to a map service layer. (E.g., http://www.example.com/arcgis/rest/services/Demographic/Language/MapServer/1)
 		*/
 		constructor: function (url, options) {
 			//  Query to get feature IDs.
-			var self = this, query, queryTask;
+			var self = this, query, queryTask, totalGroups, groupsCompleted = 0, groupsErrored = 0, featureSets = [];
+
+
+
+			/** Checks to see if the queries for all object id groups have completed.
+			*/
+			function checkForCompleted() {
+				var combinedFeatureSet;
+				if (groupsCompleted + groupsErrored === totalGroups) {
+					combinedFeatureSet = combineFeatureSets(featureSets);
+					self.emit("all-queries-complete", {
+						featureSet: combinedFeatureSet,
+						errorCount: groupsErrored
+					});
+				}
+
+			}
 
 			// Create the options object if it does not already exist.
 			if (!options) {
@@ -31,15 +66,21 @@ define([
 			query.where = "1=1";
 
 			queryTask = new QueryTask(url);
-			queryTask.on("complete", function (featureSet) {
-				self.emit("query-group-complete", featureSet);
+			queryTask.on("complete", function (event) {
+				groupsCompleted++;
+				featureSets.push(event.featureSet);
+				self.emit("query-group-complete", event);
+				checkForCompleted();
 			});
 			queryTask.on("error", function (error) {
+				groupsErrored++;
 				self.emit("query-error", error);
+				checkForCompleted();
 			});
 
 			queryTask.executeForIds(query, function (/**{Number[]}*/ objectIds) {
 				var i, l, idGroups = [], currentGroup;
+
 
 				self.emit("query-object-ids-complete", objectIds);
 
@@ -60,6 +101,9 @@ define([
 						currentGroup.push(objectIds[i]);
 					}
 				}
+
+				// Store the total number of object id groups.
+				totalGroups = idGroups.length;
 
 				for (i = 0, l = idGroups.length; i < l; i += 1) {
 					currentGroup = idGroups[i];
