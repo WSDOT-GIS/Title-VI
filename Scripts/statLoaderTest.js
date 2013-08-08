c911deec36fbb2f609eed7d16edfc514de4ea6e9
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-08-08 15:26:58 -0700

    Added list of messages to statLoaderTest.html.

--- Scripts/statLoaderTest.js
+++ Scripts/statLoaderTest.js
@@ -2,51 +2,75 @@
 /*jslint browser:true*/
 require(["esri/config", "title6/statLoader"], function (config, StatLoader) {
 	"use strict";
-	var statLoader, textArea;
+	var statLoader;
 
-	/** Gets the URL specified by the "url" query string parameter, or null if there is no such parameter.
-	@returns {String|null}
+	/** Gets a query string parameter.
+	@returns {String|null} Returns the value of the query string parameter, or null if that parameter is not defined.
 	*/
-	function getUrlQSParameter() {
-		var urlRe, match, output = null;
+	function getQueryStringParameter(/** {String} */ key) {
+		var keyRe, match, output = null;
 		if (document.location.search.length) {
-			urlRe = /url=([^\&]+)/gi;
-			match = document.location.search.match(urlRe);
+			keyRe = new RegExp(key + "=([^\\&]+)", "i");
+			match = document.location.search.match(keyRe);
 			if (match) {
 				output = match[1];
-				console.log("match found", output);
+				console.log(key, output);
 			}
 		}
 		return output;
 	}
 
-	textArea = document.getElementById("output");
+	/** Adds a message to the list on the page.
+	 * @param {String} message
+	 * @param {String|Array} [messageClass]
+	 */
+	function addMessage(message, messageClass) {
+		var li, messageList;
+		messageList = document.getElementById("messages");
+		li = document.createElement("li");
+		li.textContent = message;
+		if (messageClass) {
+			if (typeof messageClass === "string") {
+				li.setAttribute("class", messageClass);
+			} else if (messageClass instanceof Array) {
+				li.setAttribute("class", messageClass.join(" "));
+			}
+		}
+		messageList.appendChild(li);
+	}
+
 
 	config.defaults.io.proxyUrl = "proxy.ashx";
 
-	statLoader = new StatLoader(getUrlQSParameter() || "http://hqolymgis99t/arcgis/rest/services/Demographic/Language/MapServer/1");
+	statLoader = new StatLoader(getQueryStringParameter("url") || "http://hqolymgis99t/arcgis/rest/services/Demographic/Language/MapServer/1", {
+		where: getQueryStringParameter("where")
+	});
 	statLoader.on("query-object-ids-complete", function (objectIds) {
-		console.log("Object IDs query complete", objectIds);
+		// console.log("Object IDs query complete", objectIds);
+		addMessage("Object IDs query complete.");
 	});
 
 	statLoader.on("query-object-ids-error", function (error) {
-		console.error("Object IDs query error", error);
+		//console.error("Object IDs query error", error);
+		addMessage("Object IDs query error" + "\n" + JSON.stringify(error), "error");
 	});
 
 	statLoader.on("query-group-complete", function (featureSet) {
-		console.log("query group complete", featureSet);
+		//console.log("query group complete", featureSet);
+		addMessage("Query group complete");
 	});
 
 	statLoader.on("query-error", function (error) {
-		console.error("query-error", error);
+		//console.error("query-error", error);
+		addMessage("Query error" + "\n" + JSON.stringify(error), "error");
 	});
 
 	statLoader.on("all-queries-complete", function (results) {
 		if (results.errorCount) {
-			console.log(["Queries completed with", results.errorCount, "errors"].join(" "));
+			//console.log(["Queries completed with", results.errorCount, "errors"].join(" "));
+			addMessage(["Queries completed with", results.errorCount, "errors"].join(" "), "error");
 		} else {
-			console.log("All queries completed");
+			addMessage("All queries completed.");
 		}
-		textArea.value += JSON.stringify(results.featureSet);
 	});
 });
